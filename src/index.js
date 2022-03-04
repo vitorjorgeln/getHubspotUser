9b0179bb2fd8609c92a83f32642aeaf1be2bb3e2
Vitor Leal <vitorjorgeln@gmail.com>
2022-03-04 01:37:53 -0300

    Setting erros for wrong e-mail and hapikey

--- src/index.js
+++ src/index.js
@@ -11,23 +11,41 @@ app.post('/gethubspotuser/email/:email/', jsonParser, async (req, res) => {
         const params = req.params
         const headers = req.headers
         const { data } = await axios.get(url + params.email + '/profile?hapikey=' + headers.hapikey + '')
+                .catch(function (error) {
+                        if (error.response) {
+                                var erro = {
+                                        "status": "error",
+                                        "message": error.response.data.message
+                                }
+                                return res.status(error.response.status).json(erro)
+                        }
+                })
 
-        var retorno = []
+        if (data) {
+                var retorno = []
 
-        requests.params.forEach(param => {
-                retorno.push(data.properties[param].value)
-        });
+                requests.params.forEach(param => {
+                        retorno.push(data.properties[param].value)
+                });
 
-        const obj = Object.assign({}, retorno);
+                const obj = Object.assign({}, retorno);
 
-        for (let index = 0; index < requests.params.length; index++) {
+                for (let index = 0; index < requests.params.length; index++) {
 
-                obj[requests.params[index]] = obj[index]
-                delete obj[index]
+                        obj[requests.params[index]] = obj[index]
+                        delete obj[index]
+                }
+
+                return res.json(obj)
         }
 
-        return res.json(obj)
-})
 
+
+
+
+
+
+
+})
 const PORT = process.env.PORT || 666
 app.listen(PORT)
